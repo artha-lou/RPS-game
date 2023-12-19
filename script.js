e63b93b59579e062315c7493f334d4ed93c3604f
artha-lou <arthalou.projects@gmail.com>
2023-12-19 16:27:10 -0600

    gameplay function taken out. Transitioning from console interaction to interactive ui

--- script.js
+++ script.js
@@ -7,27 +7,7 @@ let cnum = "";
 let playerLives = 5;
 let computerLives = 5;
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        return("Draw, try again!")
-    } else if (playerSelection == 1 & computerSelection == 2) {
-        return("Computer wins!");
-    } else if (playerSelection == 1 & computerSelection == 3) {
-        return("Player wins!");
-    } else if (playerSelection == 2 & computerSelection == 1) {
-        return("Player wins!");
-    } else if (playerSelection == 2 & computerSelection == 3) {
-        return("Computer wins!"); 
-    } else if (playerSelection == 3 & computerSelection == 1) {
-        return("Computer wins!");
-    } else if (playerSelection == 3 & computerSelection == 2) {
-        return("Player wins!");
-    } else {
-        return("Please try again");
-    }
-}
-
-function convert(playerSelection, computerSelection) {
+function select(playerSelection, computerSelection) {
     playerSelection = prompt("rock, paper, scissors?")
     computerSelection = (Math.floor(Math.random()*3)+1);
 
@@ -50,30 +30,50 @@ function convert(playerSelection, computerSelection) {
     }
 }
 
-function game(playerSelection, computerSelection) {
-while (playerLives != 0 && computerLives != 0) {
-
-    const choices = convert(playerSelection, computerSelection)
-    const result = playRound(playerSelection, computerSelection)
-
-    if (result === "Player wins!") {
-        computerLives--;
-    } else if (result === "Computer wins!") {
-        playerLives--;
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return("Draw, try again!")
+    } else if (playerSelection == 1 & computerSelection == 2) {
+        return("Computer wins!");
+    } else if (playerSelection == 1 & computerSelection == 3) {
+        return("Player wins!");
+    } else if (playerSelection == 2 & computerSelection == 1) {
+        return("Player wins!");
+    } else if (playerSelection == 2 & computerSelection == 3) {
+        return("Computer wins!"); 
+    } else if (playerSelection == 3 & computerSelection == 1) {
+        return("Computer wins!");
+    } else if (playerSelection == 3 & computerSelection == 2) {
+        return("Player wins!");
     } else {
-        console.log("It's a draw!")
+        return("Please try again");
     }
-    console.log(result)
-    console.log("Computer Lives:", computerLives, "Computer Selection:", cnum)
-    console.log("Player Lives:", playerLives, "Player Selection:", pnum)
+}
 
-    if (playerLives === 0) {
-        return("Computer is the winner!");
-    } else if (computerLives === 0) {
-        return("Player is the winner!")
-    } 
-}
-}
+/*function game(playerSelection, computerSelection) {
+    while (playerLives != 0 && computerLives != 0) {
+        const select = select(playerSelection, computerSelection)
+        const result = playRound(playerSelection, computerSelection)
+
+        if (result === "Player wins!") {
+         computerLives--;
+        } else if (result === "Computer wins!") {
+         playerLives--;
+        } else {
+         console.log("It's a draw!")
+        }
+
+        console.log(result)
+        console.log("Computer Lives:", computerLives, "Computer Selection:", cnum)
+        console.log("Player Lives:", playerLives, "Player Selection:", pnum)
+
+        if (playerLives === 0) {
+          return("Computer is the winner!");
+        } else if (computerLives === 0) {
+          return("Player is the winner!")
+        } 
+    }
+} */
 
 const resultValue = game()
 console.log(resultValue)
