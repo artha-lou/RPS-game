79124143e724fcee2262ae29279e4c610db878ac
artha-lou <arthalou.projects@gmail.com>
2023-12-21 22:55:23 -0600

    updated code for cohesive look

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let playerLives = 5;
 let computerLives = 5;
 let resultPlayRound;
 let resultUpdateLives
+let resultWinner
 
 function select(pnum) {
     computerSelection = (Math.floor(Math.random()*3)+1);
@@ -57,40 +58,40 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-const rBtn = document.querySelector('#rBtn');
-const pBtn = document.querySelector('#pBtn');
-const sBtn = document.querySelector('#sBtn');
-const resultDiv = document.getElementById('resultDiv');
-
 function updateLives(resultPlayRound) {
     if (resultPlayRound === "Player wins!") {
         computerLives--;
     } else if (resultPlayRound === "Computer wins!") {
         playerLives--;
     } else {
-    }
 
+    }
+}
+
+function declareWinner(computerLives, playerLives) {
     if (playerLives === 0) {
         return("Computer is the winner!");
-      } else if (computerLives === 0) {
+    } else if (computerLives === 0) {
         return("Player is the winner!")
-      } 
+    } 
 }
-    
 
-function updateResult(pnum, resultPlayRound, cnum) {
+function updateResult(pnum, resultPlayRound, cnum, resultWinner) {
     // Create elements
     const playerElement = document.createElement('p');
     const computerElement = document.createElement('p');
     const resultElement = document.createElement('p');
     const playerLivesElement = document.createElement('p');
     const computerLivesElement = document.createElement('p')
+    const winnerElement = document.createElement('p')
+
     // Set text content
     playerElement.textContent = `Player selected: ${pnum}`;
     computerElement.textContent = `Computer selected: ${cnum}`;
     resultElement.textContent = resultPlayRound;
     playerLivesElement.textContent = `Player Lives: ${playerLives}`;
     computerLivesElement.textContent = `Computer Lives: ${computerLives}`;
+    winnerElement.textContent = resultWinner;
 
     // Clear existing content
     resultDiv.innerHTML = '';
@@ -101,59 +102,37 @@ function updateResult(pnum, resultPlayRound, cnum) {
     resultDiv.appendChild(resultElement);
     resultDiv.appendChild(playerLivesElement);
     resultDiv.appendChild(computerLivesElement);
+    resultDiv.appendChild(winnerElement);
 }
 
-// Example usage
-updateResult('rock', 'Player wins!', 'scissors');
+const rBtn = document.querySelector('#rBtn');
+const pBtn = document.querySelector('#pBtn');
+const sBtn = document.querySelector('#sBtn');
+const resultDiv = document.getElementById('resultDiv');
 
 rBtn.addEventListener("click", () => {
     pnum = "rock";
     resultSelect = select(pnum)
     resultPlayRound = playRound(playerSelection, computerSelection);
     resultUpdateLives = updateLives(resultPlayRound)
-    updateResult(pnum, resultPlayRound, cnum);
+    resultWinner = declareWinner(computerLives, playerLives)
+    updateResult(pnum, resultPlayRound, cnum, resultWinner);
 })
 
 pBtn.addEventListener("click", () => {
     pnum = "paper";
     resultSelect = select(pnum);
     resultPlayRound = playRound(playerSelection, computerSelection);
-    updateResult(pnum, resultPlayRound, cnum);
+    resultUpdateLives = updateLives(resultPlayRound)
+    resultWinner = declareWinner(computerLives, playerLives)
+    updateResult(pnum, resultPlayRound, cnum, resultWinner);
 })
 
 sBtn.addEventListener("click", () => {
     pnum = "scissors";
     resultSelect = select(pnum);
     resultPlayRound = playRound(playerSelection, computerSelection);
-    updateResult(pnum, resultPlayRound, cnum);
+    resultUpdateLives = updateLives(resultPlayRound)
+    resultWinner = declareWinner(computerLives, playerLives)
+    updateResult(pnum, resultPlayRound, cnum, resultWinner);
 })
-
-
-
-/*function game(playerSelection, computerSelection) {
-    while (playerLives != 0 && computerLives != 0) {
-        const select = select(playerSelection, computerSelection)
-        const result = playRound(playerSelection, computerSelection)
-
-        if (result === "Player wins!") {
-         computerLives--;
-        } else if (result === "Computer wins!") {
-         playerLives--;
-        } else {
-         console.log("It's a draw!")
-        }
-
-        console.log(result)
-        console.log("Computer Lives:", computerLives, "Computer Selection:", cnum)
-        console.log("Player Lives:", playerLives, "Player Selection:", pnum)
-
-        if (playerLives === 0) {
-          return("Computer is the winner!");
-        } else if (computerLives === 0) {
-          return("Player is the winner!")
-        } 
-    }
-} 
-
-const resultValue = game()
-console.log(resultValue)*/
